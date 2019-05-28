a8231f1aea5a93dc6d17d00db8fb8d5ca3ffffa1
Arash <arashgolmohammadi@gmail.com>
2019-05-28 17:12:30 +0430

    adding last frame view dynamically

--- app/src/main/java/com/arash/bowlingcalculator/bowling/FrameCard.kt
+++ app/src/main/java/com/arash/bowlingcalculator/bowling/FrameCard.kt
@@ -6,6 +6,7 @@ import android.view.Gravity
 import android.widget.LinearLayout
 import android.view.LayoutInflater
 import com.arash.bowlingcalculator.R
+import com.arash.bowlingcalculator.util.util.NUMBER_OF_FRAMES
 
 /**
  * Created by Arash Golmohammadi on 5/16/2019.
@@ -13,7 +14,7 @@ import com.arash.bowlingcalculator.R
 
 class FrameCard @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0, defStyleRes: Int = 0):LinearLayout(context, attrs, defStyleAttr, defStyleRes){
 
-    var frameCount = 9
+    var frameCount = 10
 
     init {
         orientation = HORIZONTAL
@@ -21,13 +22,19 @@ class FrameCard @JvmOverloads constructor(context: Context, attrs: AttributeSet?
 
         attrs?.let {
             val attr = context.obtainStyledAttributes(attrs, R.styleable.frame_cards, 0, 0)
-            frameCount = attr.getInt(R.styleable.frame_cards_frameCount,9)
+            frameCount = attr.getInt(R.styleable.frame_cards_frameCount,frameCount)
         }
-
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         for(i in 1..frameCount){
-            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            var childView = inflater.inflate(R.layout.view_card_frame, this, false)
-            addView(childView)
+            when{
+                i < frameCount -> {
+                    var childView = inflater.inflate(R.layout.view_card_frame, this, false)
+                    addView(childView)
+                }else -> {
+                    var childView = inflater.inflate(R.layout.view_card_frame_result, this, false)
+                    addView(childView)
+                }
+            }
         }
     }
 }
